610a0e2771a623ea26d53a6f865ed60909e8de5d
Gabriel Arcila <gabriel_jesus1122@hotmail.com>
2026-03-17 18:06:31 -0400

    modification of the user class

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,72 @@
 from typing import Optional
 
 from sqlmodel import Field, SQLModel
+from pydantic import BaseModel
 
 
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    email: str = Field(index=True, unique=True)
+# class User(SQLModel, table=True):
+#     id: Optional[int] = Field(default=None, primary_key=True)
+#     email: str = Field(index=True, unique=True)
+#     hashed_password: str
+#     is_active: bool = Field(default=True)s
+#     full_name: Optional[str] = None
+
+
+class User(BaseModel):
+    id: int
+    email: str
     hashed_password: str
-    is_active: bool = Field(default=True)
-    full_name: Optional[str] = None
+    is_active: bool
+    full_name: str | None
+
+    def __init__(
+        self,
+        id: int,
+        email: str,
+        hashed_password: str,
+        is_active: bool,
+        full_name: str | None,
+    ):
+        self.id = id
+        self.email = email
+        self.hashed_password = hashed_password
+        self.is_active = is_active
+        self.full_name = full_name
+
+    def __str__(self):
+        return f"User(id={self.id}, email={self.email},is_active={self.is_active}"
+
+    @property
+    def id(self):
+        return self.id
+
+    @property
+    def email(self):
+        return self.email
+
+    @property
+    def hashed_password(self):
+        return self.hashed_password
+
+    @property
+    def is_active(self):
+        return self.is_active
+
+    @property
+    def full_name(self):
+        return self.full_name
+
+    @full_name.setter
+    def full_name(self, full_name_new: str):
+        if (
+            full_name_new is not None
+            and isinstance(full_name_new, str)
+            and len(full_name_new) > 0
+        ):
+            self.full_name = full_name_new
+            return "Saved"
+        else:
+            return "Error"
 
 
 class TipUser:
