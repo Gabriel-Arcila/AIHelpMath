a0b9a2d8fcdedcad3d7a8af55bca1873e1e624bf
Gabriel Arcila <gabriel_jesus1122@hotmail.com>
2026-04-03 19:00:34 -0400

    Fix error in __init__.py

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import explanation
+from app.api.v1 import explanation
 
 api_router = APIRouter()
-api_router.include_router(explanation.router, prefix="/explanation", tags=["explanation"])
+api_router.include_router(
+    explanation.router, prefix="/explanation", tags=["explanation"]
+)
